bcab44966007534cc2ae0b045f52e52380b1eed7
steven.yeo <steven.y@dynrtech.com>
2023-07-23 17:06:47 +0700

    20230723 adding JWT

--- postgres/seeds/user-data.sql
+++ postgres/seeds/user-data.sql
@@ -1,6 +1,9 @@
 BEGIN TRANSACTION;
 
 INSERT INTO USERS (name, email, entries, joined)
-VALUES ('Steven', 'stevenyeo70@gmail.com', 8, '2023-01-01');
+VALUES ('yeoman', 'yeomandeveloper@gmail.com', 0, '2023-07-20');
+
+INSERT INTO LOGIN (hash, email)
+VALUES ('$2a$10$rM2CRlldR0etyfjSRVXgYOzbviyboLUd/64HHx.1zR.2bOXJYDZY2', 'yeomandeveloper@gmail.com');
 
 COMMIT;
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const bodyParser = require('body-parser'); // latest version of exressJS now comes with Body-Parser!
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
@@ -23,9 +22,10 @@ app.use(cors())
 app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
 
 app.get('/', (req, res)=> { res.send('Its Damm Working') })
-app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/signin', signin.signInAuthentication(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+app.post('/profile/:id', (req, res) => { profile.handleProfileUpdate(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
